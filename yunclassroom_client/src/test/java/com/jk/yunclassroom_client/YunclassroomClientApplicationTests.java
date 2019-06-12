3d1137daf866fcda2fac75dd829770fdc432755b
ssss <1176344552@qq.com>
2019-06-12 11:57:33 +0800

    第一次提交

---
+++ yunclassroom_client/src/test/java/com/jk/yunclassroom_client/YunclassroomClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jk.yunclassroom_client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YunclassroomClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
